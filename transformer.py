fa956aa7141f28bf65fd3a5830c895c2ed6fdfa3
Palaash Agrawal <palaashagrawal29@gmail.com>
2024-02-25 18:21:18 +0800

    GPT architecture with GPT4 tokenizer)

--- transformer.py
+++ transformer.py
@@ -1,6 +1,8 @@
+#TODO: parallelization of model weights across GPUs for larger models. 
+
 import torch
 import torch.nn as nn
-import torch.nn.Functional as F
+import torch.nn.functional as F
 
 
 # hyperparameters set 1: data details. 
@@ -18,9 +20,48 @@ eval_iters = 200
 
 #hyperparameters set 2: architecture details
 n_embd = 384 #which is the vocabulary size
-n_head = 6
+n_head = 6 #ie head_size = 384/6 = 64
 n_layer = 6
 # ------------
+
+class charTokenizer():
+    def __init__(self, input_file_path):
+        with open(input_file_path, 'r', encoding='utf-8') as f:
+            self.text = f.read()
+    
+        chars =sorted(list(set(self.text)))
+        self.vocab_size = len(chars)
+    
+        self.stoi = {ch:i for i,ch in enumerate(chars)}
+        self.itos = {i:ch for i,ch in enumerate(chars)}
+    
+    def encode(self, s):
+        f'takes a string, returns list of integers'
+        return [self.stoi[c] for c in s] #takes a string, ouputs a list of integers
+    
+    def decode(self, l):
+        f'takes a list of token numbers, converts to string'
+        return ''.join([self.itos[i] for i in l]) #
+    
+
+
+tokenizer = charTokenizer('input.txt')
+vocab_size = tokenizer.vocab_size
+
+
+
+#what if we use actual GPT3.5 model tokenizer?
+
+import tiktoken
+enc = tiktoken.get_encoding("cl100k_base")
+vocab_size = enc.n_vocab
+print('vocab_size: ', vocab_size)
+
+
+
+
+
+#____________________________________________________________________________________________________________________________________________
 
 
 
@@ -34,7 +75,7 @@ class Head(nn.Module):
         self.key = nn.Linear(n_embd, head_size) #by default initialization, variance is 1. 
         self.query = nn.Linear(n_embd, head_size)
         self.value = nn.Linear(n_embd, head_size)
-        self.register_buffer('tril', torch.tril(torch.ones(block_size), block_size)) #to constrain flow of info from future to current tokens. Important in autoregressive systems. 
+        self.register_buffer('tril', torch.tril(torch.ones(block_size, block_size))) #to constrain flow of info from future to current tokens. Important in autoregressive systems. 
         
 
         self.dropout = nn.Dropout(dropout)
@@ -96,7 +137,9 @@ class FeedForward(nn.Module):
     
         self.net = nn.Sequential(nn.Linear(n_embd, n_embd*4), 
                                  nn.ReLU(),
-                                 nn.Linear(n_embd*4, n_embd))
+                                 nn.Linear(n_embd*4, n_embd),
+                                 nn.Dropout(dropout),
+                                 )
         
     
     def forward(self, x): return self.net(x)
@@ -110,7 +153,6 @@ class Block(nn.Module):
         super().__init__()
         head_size = n_embd//n_head
         self.sa = MultiHeadAttention(n_head, head_size)
-        #here, we're 
         self.f = FeedForward(n_embd)
 
         self.layernorm1 = nn.LayerNorm(n_embd) #the layer referred to as Add & Norm in the original transformer block diagram
@@ -124,8 +166,11 @@ class Block(nn.Module):
         Most architectural design choices from the original transformers paper havent changed since the paper came out. 
         But a few changes have been changed in practice, because they have shown better results. 
         One of such changes is the placement of the layernorm (Add&Norm) layer in the transformer block. 
-        Instead of AFTER the multihead attention and feedforward layer, the layernorm, in practice, is now usually added BEFORE these layers. 
-        This is called the PreNormalization. Most recent papers (including ViT paper, for example), now use prenorm, instead of postnorm. 
+        Instead of AFTER the multihead attention and feedforward layer, the layernorm, in practice, is now usually added BEFORE these layers. This is called the PreNormalization. 
+
+        Source: Original GPT2 paper (language models are unsupervised multitask learners) mentions, (which was also used by the GPT3 paper (Language models are few shot learners):
+           (section 2.3 of GPT2 paper) "The model largely follows ...with a few modifications. Layer Norm was moved to the input of each sub-block, similar to pre-activation residual network " 
+        
         """
         x = x+ self.sa(self.layernorm1(x)) #in the tfm block, you see one residual connection in the multihead attention part, 
         x = x+ self.f(self.layernorm2(x)) #and another residual connection in the the feedforward part
@@ -134,4 +179,93 @@ class Block(nn.Module):
 
 
 
+class GPTLanguageModel(nn.Module):
+    name = 'GPT'
+    def __init__(self):
+        super().__init__()
+        
+        self.token_embedding_table = nn.Embedding(vocab_size, n_embd)
+        self.position_embedding_table = nn.Embedding(block_size, n_embd)
 
+        self.blocks = nn.Sequential(*[Block(n_embd, n_head = n_head) for _ in range(n_layer)])
+        
+        self.ln_f = nn.LayerNorm(n_embd) #final layer norm: 
+        #According to original GPT2 paper "Language Models are Unsupervised Multitask Learners", section 2.3: an additional layer normalization was added after the final self-attention block. 
+
+        #final linear layer to project embeddings back to one hot encoded vectors for decoding.
+        self.lm_head = nn.Linear(n_embd, vocab_size)
+
+        #better initialization for transformers. Weights are normally initialized, bias is initialized to zeros
+        self.apply(self._init_weights)
+    
+    
+        
+    def _init_weights(self, module):
+        if isinstance(module, nn.Linear):
+            torch.nn.init.normal_(module.weight, mean=0.0, std =0.02)
+            if module.bias is not None:
+                torch.nn.init.zeros_(module.bias)
+            
+        if isinstance(module, nn.Embedding):
+            torch.nn.init.normal_(module.weight, mean =0.0, std =0.02) #this initialization also comes from GPT2 paper. But how does 0.02 come?
+    
+
+    def forward(self, idx, targets =None):
+        B,T = idx.shape
+        
+        #idx and targets are both (B,T) tensors of integers
+        tok_emb = self.token_embedding_table(idx) #(B,T,C)
+        pos_emb = self.position_embedding_table(idx) #(B,T,C)
+
+        x = tok_emb + pos_emb #(B,T,C)
+        x = self.blocks(x) #(B,T,C)
+        x = self.ln_f(x) #(B,T,C)
+        logits = self.lm_head(x) #(B,T,C)
+
+        if targets is None: loss=None
+        else: 
+            B,T,C = logits.shape
+            logits = logits.view(B*T, C)
+            targets = targets.view(B*T)
+            loss = F.cross_entropy(logits, targets)
+
+        return logits, loss
+
+    def generate(self, idx, max_new_tokens = 200):
+        f"""idx is (B,T) array of indices in the current context. 
+        this function auto-regressively continues character prediction. 
+
+        We take out one prediction 
+        """
+
+        for _ in range(max_new_tokens):
+            #crop idx to the last block_size tokens
+            idx_cond = idx[:, -block_size:] #ie, the latest blocksize or less tokens will be taken at once. Rest rejected.
+            #get preds
+            logits, loss = self(idx_cond)
+            #lets take only the last token, since thats the newly generated token, and we want to print it
+            logits = logits[:, -1, :] #becomes (B,C) --> pytorch does not retain the dimension for sigle value tensor slicing. 
+            #apply softmax to get probabilities. 
+            probs = F.softmax(logits, dim = -1) # (B,C)
+
+            #sample from the distribution: Instead of just choosing the token with maximum logit. This is done to avoid repetition of the same output everytime, and introduce variability. 
+            #this makes sense for large token embedding spaces, where there are many tokens with similar (and high) logit values. 
+
+            idx_next = torch.multinomial(probs, num_samples = 1) #(B,1)
+            #append sampled index to the running sequence
+            idx = torch.cat((idx, idx_next), dim = 1) #(B,T+1)
+        
+        return idx
+
+    
+
+
+
+    def __repr__(self): 
+        return f'{self.name} Model architecture with {sum(p.numel() for p in self.parameters())/1e6}M parameters.'
+
+
+
+model = GPTLanguageModel()
+m = model.to(device)
+print(m.__repr__)
